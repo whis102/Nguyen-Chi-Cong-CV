7ab18f932f20cf429dffd8ea1664da1410f3a0ea
whis102 <chicongnguyen.dev@gmail.com>
2024-04-14 17:32:17 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import Banner from './components/Banner/Banner'
 import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <div className='container'>
-        <Header></Header>
-      </div>
+        <Header />
+        <Banner />
     </>
   )
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Banner.scss'
+import profileImage from '../../assets/avatar.jpg'
+
+function Banner() {
+    return (
+        <div className='banner-container'>
+            <img src={profileImage} alt="Profile" className="banner-image" />
+            <h1>Nguyen Chi Cong</h1>
+            <h2>Front-end Developer</h2>
+            <a href="#cv" className='banner-btn'>My CV</a>
+        </div>
+    )
+}
+
+export default Banner
