1691a1133c877b7867d186bed25f5a908098ce81
whis102 <chicongnguyen.dev@gmail.com>
2024-07-01 20:30:56 +0700

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.scss";
 import { Link } from "@mui/material";
 
 function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  function handleScroll() {
+    if (window.scrollY > 560) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
-      <nav className="nav-bar">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <Link className="nav-link" href="#about">
-              ABOUT
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" href="#skills">
-              SKILLS
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" href="#experiences">
-              EXPERIENCES
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" href="#projects">
-              PROJECTS
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <nav className={`nav-bar ${scrolled ? "scrolled" : ""}`}>
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link className="nav-link" href="#about">
+            ABOUT
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" href="#skills">
+            SKILLS
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" href="#experiences">
+            EXPERIENCES
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" href="#projects">
+            PROJECTS
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
